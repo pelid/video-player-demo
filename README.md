add2253cb9dd7984305ab2186ff59f9602a5705c
Evgeny Evseev <pelid80@gmail.com>
2020-01-07 11:58:32 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Quickstart
+
+Install Python dependencies first:
+
+```sh
+$ pip install -r requirements.txt
+```
+
+Launch static site rendering process. It will autodetect changes in template folder:
+
+```sh
+$ python script.py
+```
+
+Open in browser [http://127.0.0.1:5500/](http://127.0.0.1:5500/).
---
+++ script.py
@@ -0,0 +1,24 @@
+from livereload import Server
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+env = Environment(
+    loader=FileSystemLoader('./templates'),
+    autoescape=select_autoescape(['html'])
+)
+
+
+def render():
+
+    template = env.get_template('index.html')
+
+    html = template.render()
+
+    with open('dist/index.html', 'w') as file:
+        file.write(html)
+
+
+server = Server()
+render()  # should always render on startup before any source changes happens
+server.watch('templates', render)
+server.serve(root='dist')
